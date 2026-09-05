e2df7bf029800995eefab49116a5202b67438456
MOHAMMAD MUAAZ <muaazksj@gmail.com>
2026-09-06 00:23:52 +0530

    Add files via upload

---
+++ Week4_3_MultiplicationTable.java
@@ -0,0 +1,16 @@
+package Muaaz;
+
+import java.util.Scanner;
+
+public class Week4_3_MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
